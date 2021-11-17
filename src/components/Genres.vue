14763f58f460ab9fb1e515af2771fa9a5a3c7720
Сулаков Антон <sulakov_as@magnit.ru>
2021-11-17 23:28:35 +0300

    initial

---
+++ src/components/Genres.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <b-table striped hover :items="items"></b-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: null
+    }
+  },
+  mounted () {
+    axios
+      .get('/genres')
+      .then(response => (this.items = response))
+  }
+}
+</script>
